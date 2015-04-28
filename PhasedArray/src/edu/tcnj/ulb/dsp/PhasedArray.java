ed23b7a5a2a8d2b8694ef7881eeeb88e760259f7
Kenny Ruddick <ruddick1@tcnj.edu>
2015-04-27 21:45:14 -0400

    Fix several program-breaking bugs in the PhasedArray

--- PhasedArray/src/edu/tcnj/ulb/dsp/PhasedArray.java
+++ PhasedArray/src/edu/tcnj/ulb/dsp/PhasedArray.java
@@ -25,7 +25,7 @@ public class PhasedArray {
 	private static final double SPEED_OF_SOUND = 1500;
 	private static final Vector3[] NODES = new Vector3[9];
 	
-	private final int[] delays = new int[9];
+	private int[] delays;
 	
 	private short[][] window;
 	private int maxAdvance;
@@ -33,25 +33,21 @@ public class PhasedArray {
 	
 	static {
 		int idx = 0;
-		for (int x = 1; x > -1; x--) {
-			for (int y = 1; y > -1; y--) {
-				Vector3 v = NODES[idx++] = Vector3.of(x, y);
+		for (int x = 1; x >= -1; x--) {
+			for (int y = 1; y >= -1; y--) {
+				Vector3 v = NODES[idx++] = Vector3.of(x, y, 0);
 				v.scale(NODE_SEPARATION);
 			}
 		}
 	}
 
 	public PhasedArray(double phi, double theta, short[][] window) {
-		setWindow(window);
-		calculateChannelDelays(phi, theta);
-	}
-
-	public void setWindow(short[][] window) {
 		this.window = window;
+		calculateChannelDelays(phi, theta);
 		calculateMaxAdvance();
 		calculateMaxDelay();
 	}
-	
+
 	/**
 	 * Combines all of the channels from the window with the appropriate phasing that corresponds
 	 * to the direction of sensitivity. The combined signal is not normalized.
@@ -59,7 +55,7 @@ public class PhasedArray {
 	 * normalized. The signal may be longer than each of the individual channels.
 	 */
 	public int[] combineChannels() {
-		int sequenceLength = maxAdvance + maxDelay + window.length;
+		int sequenceLength = maxAdvance + maxDelay + window[0].length;
 		int[] result = new int[sequenceLength];
 		for (int channel = 0; channel < window.length; channel++) {
 			short[] sequence = window[channel];
@@ -71,6 +67,17 @@ public class PhasedArray {
 		return result;
 	}
 	
+	private void calculateChannelDelays(double phi, double theta) {
+		delays = new int[window.length];
+		Vector3 plane = Vector3.of(cos(theta) * sin(phi), sin(theta) * sin(phi), cos(phi));
+		for (int i = 0; i < NODES.length; i++) {
+			Vector3 phasedNode = NODES[i].copy();
+			// TODO Find out what the "distance" parameter means in Vector#projectToPlane()
+			phasedNode.projectToPlane(plane, 0);
+			delays[i] = (int) (SAMPLE_FREQUENCY * phasedNode.getZ() / SPEED_OF_SOUND);
+		}
+	}
+
 	private void calculateMaxDelay() {
 		maxDelay = delays[0];
 		for (int i = 1; i < delays.length; i++) {
@@ -84,15 +91,5 @@ public class PhasedArray {
 			min = Math.min(min, delays[i]);
 		}
 		maxAdvance = Math.abs(min);
-	}
-	
-	private void calculateChannelDelays(double phi, double theta) {
-		Vector3 plane = Vector3.of(cos(theta) * sin(phi), sin(theta) * sin(phi), cos(phi));
-		for (int i = 0; i < NODES.length; i++) {
-			Vector3 phasedNode = NODES[i].copy();
-			// TODO Find out what the "distance" parameter means in Vector#projectToPlane()
-			phasedNode.projectToPlane(plane, 0);
-			delays[i] = (int) (SAMPLE_FREQUENCY * phasedNode.getZ() / SPEED_OF_SOUND);
-		}
 	}
 }
